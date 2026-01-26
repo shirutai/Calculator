0e95b409fb1e02d95e6daa81560078738a0485fc
shirutai <232247580+shirutai@users.noreply.github.com>
2026-01-26 16:52:06 +0100

    add a non functional calc

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a, b){
+    return a + b;
+};
+function sub(a, op, b){
+    return a - b;
+};
+function multiply(a, op, b){
+    return a * b;
+};
+function divide(a, b){
+    return a / b;
+}
+
+let plus = "+";
+let a = 10;
+let b = 10;
+let result = "1 + 1";
+console.log(result.split("+"));
+const container = document.getElementById("container");
+
+function calcCreate(){
+    document.app
+}
