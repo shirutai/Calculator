308d5a326dfa5ac137a24a0bae36b0c5835964f7
shirutai <232247580+shirutai@users.noreply.github.com>
2026-01-31 15:28:52 +0100

    make the calculator and the clear btn functinonal

--- script.js
+++ script.js
@@ -1,34 +1,75 @@
-function add(a, b){
-    return a + b;
-};
-function sub(a, b){
-    return a - b;
-};
-function multiply(a, b){
-    return a * b;
-};
-function divide(a, b){
-    return a / b;
-};
-let result = 0;
+
+const calculator = document.querySelector(".calculator");
+let calcScreen = document.getElementById("screen");
+let clearBtn = document.querySelector(".clearBtn")
+let first = "";
+let second = "";
+let operator = "";
+function screenShow (btn){
+    btn = btn.target;
+    operBtn = btn.classList.contains("oper");
+    equalBtn = btn.classList.contains("equal");
+    if(btn.tagName !== "BUTTON") return;
+    if((operBtn || equalBtn) && second != "" && operator != ""){
+        first = calculate(first, second, operator);
+        calcScreen.value = first;
+        second = "";
+        if(operBtn){
+            operator = btn.textContent;
+        }
+        return;
+    };
+    if(operBtn){
+        operator = btn.textContent;
+        return;
+    };
+    if(second === "" && equalBtn){
+        return;
+    };
+    if(operator != "" && !operBtn){
+        second += btn.textContent;
+        calcScreen.value = second;
+        return;
+    };
+    if(operator === "" && !operBtn && !equalBtn){
+        first += btn.textContent;
+        calcScreen.value = first;
+        return;
+    };
+}
+function clean (){
+    first = "";
+    second = "";
+    operator = "";
+    calcScreen.value = "";
+}
+clearBtn.addEventListener("click", clean);
+calculator.addEventListener("click", screenShow);
 function calculate(a, b, oper){
+    a = Number(a);
+    b = Number(b);
     switch (oper) {
         case "+":
-            return add(a, b);
+            return a + b;
             break;
         case "-":
-            return sub(a, b);
+            return a - b;
             break;
         case "*":
-            return multiply(a, b);
+            return a * b;
             break;
         case "/":
-            return divide(a, b);
+            if(a === 0 || b === 0){
+                clean();
+                alert("bruh lmao, u know u can't do that right?");
+                return "";
+            } else{return a / b;}
             break;
         default:
             return
             break;
-    }
+    };
 };
+
 
 
